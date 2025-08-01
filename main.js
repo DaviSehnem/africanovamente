d7d3980fb7b94e8f710d5dabd2fe509a679ba3b2
Davizinho <140724854+DaviSehnem@users.noreply.github.com>
2025-08-01 13:45:22 +0000

    naufunfa

---
+++ main.js
@@ -0,0 +1,22 @@
+const numeroSenha = document.querySelector('.parametro-senha-textos');
+let TamanhoSenha = 12;
+numeroSenha.textContent = TamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+
+botoes [0].onclick = diminuiTamanho;
+botoes [0].onclick = aumentaTamanho;
+
+function diminuiTamanho () {
+    if (TamanhoSenha > 1) {
+        TamanhoSenha --;
+    }
+    numeroSenha.textContent = TamanhoSenha;
+}
+function aumentaTamanho () {
+    if (TamanhoSenha < 20) {
+        TamanhoSenha ++;
+    }
+
+}
